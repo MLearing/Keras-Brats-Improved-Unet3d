1e80f99df96ebf64c84aede49848dc3adc6b7bac
unknown <513403849@qq.com>
2018-12-01 11:04:12 +0800

    3d unet

---
+++ brats/predict.py
@@ -0,0 +1,19 @@
+import os
+
+from train import config
+from unet3d.prediction import run_validation_cases
+
+
+def main():
+    prediction_dir = os.path.abspath("prediction")
+    run_validation_cases(validation_keys_file=config["validation_file"],
+                         model_file=config["model_file"],
+                         training_modalities=config["training_modalities"],
+                         labels=config["labels"],
+                         hdf5_file=config["data_file"],
+                         output_label_map=True,
+                         output_dir=prediction_dir)
+
+
+if __name__ == "__main__":
+    main()
